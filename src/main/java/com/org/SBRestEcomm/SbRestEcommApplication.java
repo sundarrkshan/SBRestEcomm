bd8e9061dc38fce28dbd24fadecf1d6704d7fb78
sundarrkshan <sundarrkshan@gmail.com>
2020-12-23 11:05:58 +0530

    Initial commint for the SringBoot Rest API

---
+++ src/main/java/com/org/SBRestEcomm/SbRestEcommApplication.java
@@ -0,0 +1,13 @@
+package com.org.SBRestEcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/SBRestEcomm/SbRestEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.SBRestEcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRestEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
